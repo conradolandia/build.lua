1e2504cc895a1b9a9b9e2555ad684eee09b8b697
conradolandia <andresconrado@gmail.com>
2024-03-11 19:38:03 -0500

    Improve options for output

--- build.lua
+++ build.lua
@@ -9,23 +9,25 @@ local separator = "\n"
 local error_message = space .. "failed to complete"
 local success_message = space .. "completed successfully"
 
+-- Build tree
+local base_folder = "/context/third/pauta/"
+local build_folder = "tex" .. base_folder
+local docs_folder = "doc" .. base_folder
+
 -- Generating documentation with Pandoc
 local docs_command = "pandoc"
-local docs_folder = "doc/context/third/pauta/"
 
 -- Processing with LMTX
 local build_command = "/home/andi/Apps/lmtx/tex/texmf-linux-64/bin/context"
-local build_folder = "tex/context/third/pauta/"
 local build_path = "--path=" .. docs_folder .. "," .. build_folder
 
--- Build options table
-local build_options = {
-  --"--purgeall",
-  --"--noconsole",
-  build_path
-}
+-- Do we show the build log in the terminal?
+local show_log = false
 
--- Build modes for context
+-- Do we keek those logs in the root folder?
+local delete_logs = true
+
+-- Build modes for LMTX
 local build_modes = {
     h = "--mode=letter:h",
     v = "--mode=letter:v"
@@ -33,6 +35,16 @@ local build_modes = {
 
 -- Create full options string for the build command with optional mode
 local function create_options(mode)
+    local build_options = {}
+    if show_log then
+        table.insert(build_options, "--noconsole")
+    end
+    if delete_logs then
+        table.insert(build_options, "--purgeall")
+    end
+
+    table.insert(build_options, build_path)
+
     return table.concat(build_options, space) .. space .. mode
 end
 
@@ -112,5 +124,5 @@ end
 -- Build each task in the list
 for key, task in ipairs(tasks) do
     io.write("[" .. key .. "/" .. #tasks .. "] -> ")
-    execute(task)
+    execute(task, show_log)
 end
