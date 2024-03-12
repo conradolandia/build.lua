4642736c1f728f9ec584fcb02448fc48a9be2e59
conradolandia <andresconrado@gmail.com>
2024-03-11 23:36:58 -0500

    Collect all functions in a class and implement some OOP

--- build.lua
+++ build.lua
@@ -29,8 +29,8 @@ local delete_logs = true
 
 -- Build modes for LMTX
 local build_modes = {
-    h = "--mode=letter:h",
-    v = "--mode=letter:v"
+	h = "--mode=letter:h",
+	v = "--mode=letter:v",
 }
 
 -- [[ Internal functions ]]
@@ -38,101 +38,118 @@ local build_modes = {
 -- Create full options string for the build command with optional mode
 -- I use a table because it seems easier than just concatenating strings and spaces
 local function create_options(mode)
-    local build_options = {}
-    -- Show log?
-    if show_log then
-        table.insert(build_options, "--noconsole")
-    end
-    -- Delete logs?
-    if delete_logs then
-        table.insert(build_options, "--purgeall")
-    end
-    -- Insert extra paths
-    if build_path then
-      table.insert(build_options, build_path)
-    end
-    -- Insert LMTX build mode
-    if mode then
-        table.insert(build_options, mode)
-    end
-    -- Return table as concatenated string
-    return table.concat(build_options, space)
+	local build_options = {}
+	-- Show log?
+	if show_log then
+		table.insert(build_options, "--noconsole")
+	end
+	-- Delete logs?
+	if delete_logs then
+		table.insert(build_options, "--purgeall")
+	end
+	-- Insert extra paths
+	if build_path then
+		table.insert(build_options, build_path)
+	end
+	-- Insert LMTX build mode
+	if mode then
+		table.insert(build_options, mode)
+	end
+	-- Return table as concatenated string
+	return table.concat(build_options, space)
 end
 
--- Function to execute a task
-local function execute(task, show_output)
-    print("Executing [ " .. task.name .. " ]")
+-- [[ Task Class ]]
+local Task = {}
+Task.__index = Task
 
-    local command = task.command
-
-    if task.options then
-        command = command .. space .. task.options
-    end
-
-    if task.input then
-        command = command .. space .. task.input
-    end
-
-    if task.after then
-        command = command .. space .. task.after
-    end
-
-    if task.output then
-        command = command .. space .. task.output
-    end
-
-    print("Calling: " .. command)
-
-    local handle = io.popen(command)
-    local output = handle:read("*a")
-
-    if show_output then
-      print(output)
-    end
-
-    local message
-
-    if handle then
-        message = success_message
-    end
-
-    print(intro .. task.input .. message)
-
-    if task.postprocess ~= nil then
-      local success, tag, code = os.execute(task.postprocess)
-      if success then
-        print("Postprocessing: " .. task.postprocess .. separator)
-      end
-    else
-      print(separator)
-    end
+function Task:new(name, command, options, input, after, output, postprocess)
+	local self = setmetatable({}, Task)
+	self.name = name
+	self.command = command
+	self.options = options
+	self.input = input
+	self.after = after
+	self.output = output
+	self.postprocess = postprocess
+	return self
 end
 
--- Tasks to execute and options
-local tasks = {{
-  name = "Build documentation from README.md",
-  command = docs_command,
-  options = "--from=markdown --to=context+ntb --wrap=none --top-level-division=chapter --section-divs",
-  input = "README.md",
-  after = "-o",
-  output = docs_folder .. "README.tex"
-}, {
-  name = "Typeset example",
-  command = build_command,
-  options = create_options(build_modes.h),
-  input = docs_folder .. "pauta-example.tex",
-  postprocess = "mv pauta-example.pdf " .. docs_folder
-}, {
-  name = "Typeset documentation",
-  command = build_command,
-  options = create_options(build_modes.v),
-  input = docs_folder .. "pauta.tex",
-  postprocess = "mv pauta.pdf " .. docs_folder
-}}
+function Task:execute(show_output)
+	print("Executing [ " .. self.name .. " ]")
+
+	local command = self.command
+	if self.options then
+		command = command .. space .. self.options
+	end
+	if self.input then
+		command = command .. space .. self.input
+	end
+	if self.after then
+		command = command .. space .. self.after
+	end
+	if self.output then
+		command = command .. space .. self.output
+	end
+
+	print("Calling: " .. command)
+
+	local handle = io.popen(command)
+	local output = handle:read("*a")
+
+	if show_output then
+		print(output)
+	end
+
+	local message = success_message
+	if handle then
+		message = success_message
+	end
+
+	print(intro .. self.input .. message)
+
+	if self.postprocess ~= nil then
+		local success, tag, code = os.execute(self.postprocess)
+		if success then
+			print("Postprocessing: " .. self.postprocess .. separator)
+		end
+	else
+		print(separator)
+	end
+end
+
+-- [[ Tasks ]]
+local tasks = {
+	Task:new(
+		"Build documentation from README.md",
+		docs_command,
+		"--from=markdown --to=context+ntb --wrap=none --top-level-division=chapter --section-divs",
+		"README.md",
+		"-o",
+		docs_folder .. "README.tex"
+	),
+	Task:new(
+		"Typeset example",
+		build_command,
+		create_options(build_modes.h),
+		docs_folder .. "pauta-example.tex",
+		nil,
+		nil,
+		"mv pauta-example.pdf " .. docs_folder
+	),
+	Task:new(
+		"Typeset documentation",
+		build_command,
+		create_options(build_modes.v),
+		docs_folder .. "pauta.tex",
+		nil,
+		nil,
+		"mv pauta.pdf " .. docs_folder
+	),
+}
 
 -- Build each task in the list
 for key, task in ipairs(tasks) do
-    io.write("[" .. key .. "/" .. #tasks .. "] -> ")
-    execute(task, show_log)
+	io.write("[" .. key .. "/" .. #tasks .. "] -> ")
+	task:execute(show_log)
 end
-
